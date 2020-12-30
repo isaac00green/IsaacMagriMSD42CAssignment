39f2c44fd091ec7b2cd875feb559601565457ec9
isaac00green <isaac.magri.e25464@mcast.edu.mt>
2020-12-30 16:25:55 +0100

    Player Move, Boundary, Padding
    -Added player move function
    -Added the players boundary move and padding.

---
+++ HomeAssignment/IsaacMagriCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingx =  0.6f;
+    [SerializeField] float paddingy =  0.9f;
+
+    float xMin, xMax, yMin, yMax;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingx;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - paddingx;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingy;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingy;
+    }
+
+    //moves the player car
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        this.transform.position = new Vector2(newXPos, newYPos);
+    }
+}
